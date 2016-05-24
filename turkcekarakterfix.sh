3f67c753cdbff29bdc7173d5b0bf62b58eed7216
iilkkaya <ibrahim.ilkkaya@ogr.bartin.edu.tr>
2016-05-24 13:36:21 +0300

    Update turkcekarakterfix.sh

--- turkcekarakterfix.sh
+++ turkcekarakterfix.sh
@@ -1,5 +1,5 @@
 #Fix Turkish Characters
-sudo wget https://raw.githubusercontent.com/iilkkaya/latestdeluge/master/km-041f.ini
+sudo wget --no-check-certificate https://raw.githubusercontent.com/iilkkaya/latestdeluge/master/km-041f.ini
 sudo cp km-041f.ini /etc/xrdp/
 sudo chown root:root /etc/xrdp/km-041f.ini
 sudo /etc/init.d/xrdp restart
